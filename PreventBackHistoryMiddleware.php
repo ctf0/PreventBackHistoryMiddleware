89c08a1e13a2c210ebf62ab04573b3c22aaaa5f7
Muah <muah003@gmail.com>
2016-10-08 22:53:22 +0300

    fix the statment

--- PreventBackHistoryMiddleware.php
+++ PreventBackHistoryMiddleware.php
@@ -18,7 +18,7 @@ class PreventBackHistoryMiddleware
     {
         $response = $next($request);
 
-        if (str_contains('logout', $request->url())) {
+        if (str_contains($request->url(), 'logout')) {
             return $response->header('Cache-Control', 'nocache, no-store, max-age=0, must-revalidate')
                 ->header('Pragma', 'no-cache')
                 ->header('Expires', 'Fri, 01 Jan 1990 00:00:00 GMT');
