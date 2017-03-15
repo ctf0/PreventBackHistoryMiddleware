32932ab4496672de57e6fa1adab88ff7ed4c231f
Muah <ctf0@users.noreply.github.com>
2017-03-15 16:22:21 +0200

    Update PreventBackHistoryMiddleware.php

--- PreventBackHistoryMiddleware.php
+++ PreventBackHistoryMiddleware.php
@@ -18,12 +18,9 @@ class PreventBackHistoryMiddleware
     {
         $response = $next($request);
 
-        if (str_contains($request->url(), 'logout')) {
-            return $response->header('Cache-Control', 'nocache, no-store, max-age=0, must-revalidate')
-                ->header('Pragma', 'no-cache')
-                ->header('Expires', 'Fri, 01 Jan 1990 00:00:00 GMT');
-        }
+        return $response->header('Cache-Control', 'nocache, no-store, max-age=0, must-revalidate')
+            ->header('Pragma', 'no-cache')
+            ->header('Expires', 'Fri, 01 Jan 1990 00:00:00 GMT');
 
-        return $response;
     }
 }
