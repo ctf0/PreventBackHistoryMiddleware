2ee9c5b5122e8408e157fa6a124279e39af01ab2
Muah <ctf0@users.noreply.github.com>
2017-08-20 00:30:54 +0200

    update to work on logout only
    in v5.4+ this is done internally

--- PreventBackHistoryMiddleware.php
+++ PreventBackHistoryMiddleware.php
@@ -18,9 +18,12 @@ class PreventBackHistoryMiddleware
     {
         $response = $next($request);
 
-        return $response->header('Cache-Control', 'nocache, no-store, max-age=0, must-revalidate')
-            ->header('Pragma', 'no-cache')
-            ->header('Expires', 'Fri, 01 Jan 1990 00:00:00 GMT');
+        if (str_contains($request->url(), 'logout')) {
+            return $response->header('Cache-Control', 'nocache, no-store, max-age=0, must-revalidate')
+                ->header('Pragma', 'no-cache')
+                ->header('Expires', 'Fri, 01 Jan 1990 00:00:00 GMT');
+        }
 
+        return $response;
     }
 }
